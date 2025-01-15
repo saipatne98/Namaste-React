256166be083cfb7fec0d17cd3a431b6b5ab5dacc
Saiprasad Shivaji Patne <saipatne98@gmail.com>
2025-01-15 07:49:39 +0530

    Second Chapter Completed

--- 1.Inception/App.js
+++ 1.Inception/App.js
@@ -1,27 +1,20 @@
-
-// <div>
-//     <span>
-//         <ul>sai</ul>
-//         <section>Patne</section>
-//     </span>
-// </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
+const parent= React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1",{},"This is Namaste React"),
+        React.createElement("h2",{},"I am an H2 tag"),
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am an H2 tag")
+    ])    
+]);
 
-const heading = React.createElement(
-    'div',
-    {},
-    React.createElement(
-        "span",
-        {},
-        [  
-            React.createElement("ul",{},"sai"),
-            React.createElement("section",{},"Patne")
-        ]
-)
-)
+console.log(parent); //object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
-
+root.render(parent);
